b86c2de418ea7f35cbdfdab11e0524666a837a18
pengyingxue <yingxuepeng@6tiantian.com>
2022-10-03 14:29:21 +0800

    修正脚本

--- deploy/server/deploy_server.sh
+++ deploy/server/deploy_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #/target/qqbot-2.0.0-SNAPSHOT.jar
-
+scp -r ../../cache/ ubuntu@106.53.108.169:/home/raccoon_bot/
 scp ../../target/qqbot-2.0.0-SNAPSHOT.jar ubuntu@106.53.108.169:/home/raccoon_bot/
 scp ./restart.sh ubuntu@106.53.108.169:/home/raccoon_bot/
 #scp ./check.sh ubuntu@106.53.108.169:/home/raccoon_bot/
